545301efaa7b957e6c744d184e278489fa399842
Kevin <kpluas21@gmail.com>
2023-02-15 17:29:01 -0500

    Initial Commit

---
+++ bookstore/book/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+
+#By default, shows a list of all the books in the db
+urlpatterns = [
+    path('', views.getBook),
+    path('get/', views.getBook),
+    path('post/', views.postBook),
+]
--- bookstore/book/views.py
+++ bookstore/book/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Book
+from .serializer import BookSerializer
+
+@api_view(['GET'])
+def getBook(request):
+    book = Book.objects.all()
+    serializer = BookSerializer(book, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def postBook(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
 
 # Create your views here.
