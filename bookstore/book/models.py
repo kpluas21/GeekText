0c12c0bc1df34fc9f13717158547a2f49d6338a3
Kevin <kpluas21@gmail.com>
2023-02-18 10:30:30 -0500

    Added publisher and price to the database for the other features

--- bookstore/book/models.py
+++ bookstore/book/models.py
@@ -19,11 +19,16 @@ class Book(models.Model):
     genre  = models.CharField(max_length=200)
     isbn   = models.IntegerField()
     copiesSold=models.PositiveSmallIntegerField()
+    price  = models.IntegerField()
+    publisher = models.CharField(max_length=200, null=True)
+    #rating goes here but we need to see how that will work
+    #with book rating and commenting
     
 
     def __str__(self):
         return str(self.title)
 
+#The customer class however someone else might be doing this one?
 class Customer(models.Model):
     userName = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
     email    = models.EmailField(null = True)
