014f3dd56ca0cb2d4f45d826dd233ada31c60de6
zholsy <99848082+zholsy@users.noreply.github.com>
2023-04-15 21:45:04 -0400

    Update serializers.py

--- CommentAndRate/BookCommentAndRate/serializers.py
+++ CommentAndRate/BookCommentAndRate/serializers.py
@@ -1,5 +1,38 @@
 from rest_framework import serializers
+from .models import Book
 from .models import Book, BookRating, BookComment
+
+#Our main serializer that simply grabs all the books and shows off each of their fields
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields=('title', 'author', 'year', 'isbn', 'genre', 'copiesSold', 'price', 'publisher', 'rating')
+
+#Used just to list books by their genres
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields=('title', 'author', 'genre')
+
+#Used to pull up books based on their publishers with the purpose of editing the prices
+class PublisherPatchingSerializer(serializers.ModelSerializer):
+    price = serializers.IntegerField(read_only=False)
+
+    class Meta:
+        model=Book
+        fields=('title', 'author', 'publisher', 'price',)
+
+class PriceResetSerializer(serializers.ModelSerializer):
+    price = serializers.IntegerField(read_only=False)
+
+    class Meta:
+        model=Book
+        fields=('price',)
+    
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields=('title', 'author', 'rating', 'price')
 
 class BookRatingSerializer(serializers.ModelSerializer):
     class Meta:
