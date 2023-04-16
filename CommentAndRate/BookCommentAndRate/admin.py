b0a4f50bfe99bd3e9ce3f070269c39800f978d3b
zholsy <99848082+zholsy@users.noreply.github.com>
2023-04-15 21:42:27 -0400

    Update admin.py

--- CommentAndRate/BookCommentAndRate/admin.py
+++ CommentAndRate/BookCommentAndRate/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from .models import Book, BookComment
 from . import models
+admin.site.register(Book)
+# admin.site.register(Customer)
 
 # Register your models here.
-
 admin.site.register(models.BookRating)
 admin.site.register(models.BookComment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('Username', 'Comment',)
+    date_hierarchy = 'created'
